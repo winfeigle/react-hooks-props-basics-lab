9939f56e64b4808ef025533e3bd5b663f6d6d123
Win Feigle <win.feigle@gmail.com>
2022-07-09 07:14:30 -0600

    commit tree 2a1a00e2e11c0e4e6780ffc4107bd00e2f792a49

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Win",
+  city: "Denver",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/winfeigle",
+    linkedin: "https://www.linkedin.com/in/feigle/",
   },
 };
 
